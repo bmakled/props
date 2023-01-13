1f9c5bdaf94f47acdc2edea0da2805aa9808d22d
Basiel Makled <basiel1234@gmail.com>
2023-01-13 15:27:32 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import PersonCard from './components/PersonCard';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Person Cards</h1>
+      <PersonCard 
+      name = {'Doe, Jane'}
+      age = {'45'}
+      hairColor = {'Black'}
+      />
+      <PersonCard 
+      name = {'Smith, John'}
+      age = {'88'}
+      hairColor = {'Brown'}
+      />
+      <PersonCard 
+      name = {'Fillmore, Millard'}
+      age = {'50'}
+      hairColor = {'Brown'}
+      />
+      <PersonCard 
+      name = {'Smith, Maria'}
+      age = {'62'}
+      hairColor = {'Brown'}
+      />
+    </div>
+  );
+}
+
+export default App;
